262fe14a196d6bfa9c96a6a46d28b1a1c4339368
Baine <Baine@users.noreply.github.com>
2019-02-05 13:52:18 +0100

    Revert "[I] Improve Sorting in Manually Linked Files and SeriesList in Unrecognized files"
    This reverts commit 291ba923dfa35c75394711d15a14fb363c7a7c3b.

--- Shoko.Desktop/UserControls/UnrecognisedVideos.xaml.cs
+++ Shoko.Desktop/UserControls/UnrecognisedVideos.xaml.cs
@@ -127,7 +127,6 @@ namespace Shoko.Desktop.UserControls
 
             AllSeries = new ObservableCollection<VM_AnimeSeries_User>();
             ViewSeries = CollectionViewSource.GetDefaultView(AllSeries);
-            ViewSeries.SortDescriptions.Add(new SortDescription("SortName", ListSortDirection.Ascending));
             ViewSeries.Filter = SeriesSearchFilter;
 
             btnRefresh.Click += new RoutedEventHandler(btnRefresh_Click);
